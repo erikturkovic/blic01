fe4232eaca981cab63c8c2244b086986e72934c9
erikturkovic <eturkovic@student.unipu.hr>
2022-04-01 09:23:59 +0200

    Back button :)

--- src/views/Detalji.vue
+++ src/views/Detalji.vue
@@ -1,27 +1,22 @@
 <template>
   <div class="about">
-    {{name}}
-      <li v-for="book in books" v-if="book.name == name" v-bind:key="book.name">
-        
-        ISBN -> {{book.isbn}}
-      </li>
-     
+    {{ name }}
+    <li v-for="book in books" v-if="book.name == name" v-bind:key="book.name">
+      ISBN -> {{ book.isbn }}
+    </li>
   </div>
 </template>
 
-
 <script>
 export default {
-props:["name"],
+  props: ["name"],
   data: function () {
     return {
       books: [],
     };
   },
   async mounted() {
-    let rezultat = await fetch(
-      "https://www.anapioficeandfire.com/api/books"
-    );
+    let rezultat = await fetch("https://www.anapioficeandfire.com/api/books");
     let podaci = await rezultat.json();
     console.log(podaci);
     this.books = podaci;
