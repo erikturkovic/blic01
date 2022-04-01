90bef8e47dfe39c311777568446e7e918b510b0e
erikturkovic <eturkovic@student.unipu.hr>
2022-04-01 09:21:00 +0200

    Sređen home page i router linkovi
    Sređen home page i pocinjem slagati Details.vue

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,10 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
+    path: '/book/:name',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/Detalji.vue'),
+    props:true
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
-
+    <li v-for="book in books" v-bind:key="book.name">Ime autora = {{book.authors}}
+      <router-link :to="'/book/' + book.name">, Ime knjige = {{
+         book.name
+        }}</router-link> , Datum izlaska = 
+        {{book.released}}
+    </li>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: "HomeView",
   data: function () {
@@ -15,9 +18,7 @@ export default {
     };
   },
   async mounted() {
-    let rezultat = await fetch(
-      "https://www.anapioficeandfire.com/api/books"
-    );
+    let rezultat = await fetch("https://www.anapioficeandfire.com/api/books");
     let podaci = await rezultat.json();
     console.log(podaci);
     this.books = podaci;
